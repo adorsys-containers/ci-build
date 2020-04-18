a3d7611414fd9a84a5a73baabac0914a2a35f25a
Jan-Otto Kröpke <mail@jkroepke.de>
2020-04-18 22:42:44 +0200

    Unpin UBI Version

--- 2/generated/ubi-8-java-11-node-10/tests/tests.sh
+++ 2/generated/ubi-8-java-11-node-10/tests/tests.sh
@@ -8,6 +8,8 @@ docker run --rm "${DOCKER_IMAGE}:${TAG}" bash --version
 docker run --rm -eDOCKER_CLI_EXPERIMENTAL=enabled "${DOCKER_IMAGE}:${TAG}" docker buildx version
 docker run --rm "${DOCKER_IMAGE}:${TAG}" docker --version
 docker run --rm "${DOCKER_IMAGE}:${TAG}" git --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" which --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" which git
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'date | grep -E "CES?T"'
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'locale | grep -E LC_ALL=.+\.UTF-8'
